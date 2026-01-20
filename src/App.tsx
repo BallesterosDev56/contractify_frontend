246854750120ad0504eb276508fda5dd7b3750d2
BallesterosDev56 <ballesterosdeveloper56@gmail.com>
2026-01-20 00:46:56 -0500

    Add console log for Firebase API key in App component for debugging purposes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { useRoutes } from 'react-router-dom';
 import { routesConfig } from '@/routes/routes';
 
 export const App = () => {
+  console.log(import.meta.env.VITE_FIREBASE_API_KEY)
+
   const routes = useRoutes(routesConfig);
   return routes;
 };
