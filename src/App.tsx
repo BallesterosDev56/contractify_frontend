0b111445729761003542a5115f7f2afa700e36cb
BallesterosDev56 <ballesterosdeveloper56@gmail.com>
2026-01-20 01:17:22 -0500

    Add Firebase authentication state listener in App component to log JWT token or unauthenticated status

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,18 @@
 
 import { useRoutes } from 'react-router-dom';
 import { routesConfig } from '@/routes/routes';
+import { onAuthStateChanged } from 'firebase/auth';
+import { firebaseAuth } from './auth/firebase';
 
 export const App = () => {
+  onAuthStateChanged(firebaseAuth, async (user) => {
+    if (user) {
+      const token = await user.getIdToken();
+      console.log("JWT:", token);
+    } else {
+      console.log("No autenticado");
+    }
+  });
   const routes = useRoutes(routesConfig);
   return routes;
 };
